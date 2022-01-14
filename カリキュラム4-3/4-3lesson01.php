1c10f06cf5fc29508842940f0692849c317945fc
橋本玲旺 <96459793+reo0920@users.noreply.github.com>
2022-01-14 17:10:04 +0900

    Add files via upload

---
+++ カリキュラム4-3/4-3lesson01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>世界のナベアツプログラム</title>
+</head>
+<body>
+    <section>
+        <!-- ここに結果表示 -->
+        <?php
+        include('lesson01.php');
+        nabeatsu(isset($i));
+        ?>
+    </section>
+</body>
+</html>
